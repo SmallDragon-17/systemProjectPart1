6e526c1d209441605a5b96b0ff4b4ce1076a0a95
r236015k <r236015k@pc11.ces.kyutech.local>
2019-04-26 14:24:12 +0900

    Fix FeatureLefLinerity

--- src/FeatureLeftLinerity.java
+++ src/FeatureLeftLinerity.java
@@ -2,7 +2,6 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
-
 //
 //	文字画像の左辺の直線度を特徴量として計算するクラス
 //
@@ -12,11 +11,20 @@ class  FeatureLeftLinerity implements FeatureEvaluater
 	protected float  left_length;
 	protected float  char_height;
 
-	// 左側の辺（画像の各行の最も左側にあるドットのＸ座標、行にひとつもドットがなければ -1）
-	protected int  left_x[];
+	protected int left_x[];
+	protected int left_y_start;
+	protected int left_y_end;
+
+/*  5-4においては追加で必要
+	//それぞれの左の線の始まりのy座標と終わりのy座標を格納する
+	protected List<int[]> left_y_list;
+	//左の線の最も長い区間として使用するリストleft_y_listnのインデックス
+	protected int main_num;
+*/
 
 	// 最後に特徴量計算を行った画像（描画用）
 	protected BufferedImage  last_image;
+
 
 	// 特徴量の名前を返す
 	public String  getFeatureName()
@@ -30,20 +38,15 @@ class  FeatureLeftLinerity implements FeatureEvaluater
 		int  height = image.getHeight();
 		int  width = image.getWidth();
 
-		// 左側の辺を取り出す（各行の最も左側のドットのＸ座標を調べる）
 		left_x = new int[ height ];
 		for ( int y=0; y<height; y++ )
 		{
-			// 最初は行に黒ピクセルが１つもないものとして -1 で初期化
 			left_x[ y ] = -1;
 
-			// 左側から順番にピクセルを走査
 			for ( int x=0; x<width; x++ )
 			{
-				// ピクセルの色を取得
 				int  color = image.getRGB( x, y );
 
-				// ピクセルの色が黒であれば最も左側のドットとして座標を記録
 				if ( color == 0xff000000 )
 				{
 					left_x[ y ] = x;
@@ -52,12 +55,91 @@ class  FeatureLeftLinerity implements FeatureEvaluater
 			}
 		}
 
+
 		// 文字の高さを計算
-		char_height = 1.0f; // 要実装
+		//start: 配列left_xにおいて最初に-1以外となったインデックス
+		int start = 0;
+		for (int i = 0; i < height; i++) {
+			if (left_x[i] != -1) {
+				start = i;
+				break;
+			}
+		}
+		left_y_start = start;
+		//end: 配列left_xにおいて最後に-1以外となったインデックス
+		int end = 0;
+		for (int i = height-1; i > 0; i--) {
+			if (left_x[i] != -1) {
+				end = i;
+				break;
+			}
+		}
+		left_y_end = end;
+		//文字の高さ = 黒い線の終わり - 黒い線の始まり
+		char_height = end - start;
+
+/* 5-4ver.の内容
+		//ギャップのある区間の分割
+		left_y_list = new ArrayList<>();
+
+		//forループの中で使用する一時的な配列を用意
+		int left_y_tmp[] = new int[2];
+		left_y_tmp[0] = start;
+		for (int i = start; i < end; i++) {
+			//ひとつ後ろのy座標の線とのx座標の差が15より大きかったら
+			//ギャップがあるとみなす
+			if (Math.abs(left_x[i+1] - left_x[i]) > 15) {
+				left_y_tmp[1] = i;
+
+				//とりあえず分けた区間はすべてリストleft_y_listに格納する
+				left_y_list.add(left_y_tmp.clone());
+
+				left_y_tmp[0] = i + 1;
+			}
+		}
+		left_y_tmp[1] = end;
+		left_y_list.add(left_y_tmp);
+
+		//最も長い区間かどうか判定し、長かったらleft_y_mainに格納する
+		//main_len: 左の線の最も長い区間の長さ
+		//main_num: リストleft_y_listにおいて
+		//左の線の最も区間の長い配列が入っているインデックスを探す
+		int main_len = -1;
+		main_num = 0;
+		for (int i = 0; i < left_y_list.size(); i++) {
+			int tmp_array[] = left_y_list.get(i);
+
+			if (tmp_array[1] - tmp_array[0] > main_len) {
+				main_num = i;
+				main_len = tmp_array[1] - tmp_array[0];
+			}
+		}
+
 
 		// 文字の左側の辺の長さを計算
-		left_length = 1.0f; // 要実装
+		//呼び出し元は同一のインスタンスを使用しており、
+		//left_lengthはグローバルであるため、
+		//呼び出される毎にleft_lengthを0にリセットしておく
+		left_length = 0;
+		int left_y_main[] = left_y_list.get(main_num);
+		int calc_start = left_y_main[0];
+		int calc_end = left_y_main[1];
+		for (int i = calc_start; i < calc_end; i++) {
+			left_length += Math.sqrt(
+					Math.pow(Math.abs(left_x[i]-left_x[i+1]), 2) + 1
+					);
+		}
+*/
 
+
+//  5-3ver.の始まり
+		left_length = 0;
+		for (int i = start; i < end; i++) {
+			left_length += Math.sqrt(
+					Math.pow(Math.abs(left_x[i]-left_x[i+1]), 2) + 1
+					);
+		}
+//  5-3ver.の終わり
 
 		// 文字の高さ / 左側の辺の長さ の比を計算
 		float  left_linearity;
@@ -78,6 +160,7 @@ class  FeatureLeftLinerity implements FeatureEvaluater
 		int  ox = 0, oy = 0;
 		g.drawImage( last_image, ox, oy, null );
 
+//  5-3ver.の始まり
 		int  x0, y0, x1, y1;
 		for ( int y=0; y<left_x.length-1; y++ )
 		{
@@ -92,7 +175,44 @@ class  FeatureLeftLinerity implements FeatureEvaluater
 				g.drawLine( ox + x0, oy + y0, ox + x1, oy + y1 );
 			}
 		}
+//  5-3ver.の終わり
 
+/*  5-4の内容
+		int  x0, y0, x1, y1;
+		int y_start[] = new int[left_y_list.size()];
+		int y_end[] = new int[left_y_list.size()];;
+
+		//左の線の描画
+		//それぞれの区間の始まりと終わりを配列に格納
+		for (int i = 0; i < left_y_list.size(); i++) {
+			int array[] = left_y_list.get(i);
+			y_start[i] = array[0];
+			y_end[i] = array[1];
+		}
+
+		int start, end;
+		for (int i = 0; i < left_y_list.size(); i++) {
+		    start = y_start[i];
+			end = y_end[i];
+
+			for (int y = start; y < end; y++) {
+				y0 = y;
+				y1 = y+1;
+				x0 = left_x[ y0 ];
+				x1 = left_x[ y1 ];
+
+				if (i != main_num) {
+					g.setColor( Color.BLUE );
+				}
+				else {
+					g.setColor( Color.RED );
+				}
+				g.drawLine( ox + x0, oy + y0, ox + x1, oy + y1 );
+			}
+		}
+*/
+
+		// 高さの線の描画
 		String  message;
 		g.setColor( Color.RED );
 		message = "左辺の長さ: " + left_length;
@@ -101,5 +221,28 @@ class  FeatureLeftLinerity implements FeatureEvaluater
 		g.drawString( message, ox, oy + 32 );
 		message = "特徴量(文字の高さ / 左辺の長さ): " + char_height / left_length;
 		g.drawString( message, ox, oy + 48 );
+	}
+
+	public boolean leftCheck(int i) {
+		/*左の線がかけているかいないかチェックするメソッド
+		 * @param i: 線がかけ始めていると思われるleft_xのインデックス
+		 * @return: 線がかけていると判断したらtrue
+		 */
+		boolean res = false;
+		if (Math.abs(left_x[i+1] - left_x[i]) > 15) {
+			res = true;
+		}
+		//少しだけ線が切れてしまっていたりしているときの対応
+		//チェックするにあたり、配列のサイズを超えないようにしている
+		if (res && left_x.length - 3 > i) {
+			int len1 = Math.abs(left_x[i+2] - left_x[i]);
+			int len2 = Math.abs(left_x[i+3] - left_x[i]);
+
+			if (len1 < 15 || len2 < 15) {
+				res = false;
+			}
+		}
+
+		return res;
 	}
 }
